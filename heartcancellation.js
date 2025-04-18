41c55e90fd0a5e0a0f0108c432b89df58390a571
truongbm2023 <149526285+truongbm2023@users.noreply.github.com>
2025-04-18 16:26:12 -0400

    Update heartcancellation.js

--- heartcancellation.js
+++ heartcancellation.js
@@ -1,13 +1,5 @@
 /**
  *------
- * BGA framework: © Gregory Isabelli <gisabelli@boardgamearena.com> & Emmanuel Colin <ecolin@boardgamearena.com>
- * Hearts implementation : © Gregory Isabelli <gisabelli@boardgamearena.com>
- *
- * This code has been produced on the BGA studio platform for use on http://boardgamearena.com.
- * See http://en.boardgamearena.com/#!doc/Studio for more information.
- * -----
- *
- * hearts.js
  *
  * Hearts user interface script
  * 
