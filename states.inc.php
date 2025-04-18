cc8bae3c9e0500a7c1db14ac31fad2e44bfc0a41
truongbm2023 <149526285+truongbm2023@users.noreply.github.com>
2025-04-18 16:27:33 -0400

    Update states.inc.php

--- states.inc.php
+++ states.inc.php
@@ -1,12 +1,5 @@
 <?php
 /**
- *------
- * BGA framework: © Gregory Isabelli <gisabelli@boardgamearena.com> & Emmanuel Colin <ecolin@boardgamearena.com>
- * Hearts implementation : © Gregory Isabelli <gisabelli@boardgamearena.com>
- *
- * This code has been produced on the BGA studio platform for use on http://boardgamearena.com.
- * See http://en.boardgamearena.com/#!doc/Studio for more information.
- * -----
  * 
  * states.inc.php
  *
